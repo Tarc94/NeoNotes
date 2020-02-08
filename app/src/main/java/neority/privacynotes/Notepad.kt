2c005d61cee49b8f27d3a955e462d3f029386102
Tarc94 <tarc94@hotmail.co.uk>
2020-02-08 13:01:32 +0000

    Initial commit

---
+++ app/src/main/java/neority/privacynotes/Notepad.kt
@@ -0,0 +1,8 @@
+package neority.privacynotes
+
+class Notepad(nodeID:Int, nodeName:String, nodeDes:String) {
+
+    var notepadID:Int?=nodeID
+    var notepadName:String?=nodeName
+    var notepadDes:String?=nodeDes
+}
